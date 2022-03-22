3c0ae64ae16f547792c6c64cd1935c7becea803d
stebinbabu <98040227+stebinbabu@users.noreply.github.com>
2022-03-22 20:28:07 +0530

    Add files via upload

---
+++ 22-03-2022/pgrm2.java
@@ -0,0 +1,9 @@
+public class pgrm2{
+	public static void main(String args[]){
+		String str1="stebin";
+		String str2="babu";
+		String str3 = str1 + str2;
+		System.out.println(str3);
+	}
+	
+}
---
+++ 22-03-2022/pgrm3.java
@@ -0,0 +1,12 @@
+public class pgrm3{
+	public static void main(String arg[])
+	{
+		int a=5,b=3,c;
+		c=a+b;
+		System.out.println(c=a+b);
+		System.out.println(c=a-b);
+		System.out.println(c=a*b);
+		System.out.println(c=a/b);
+		System.out.println(c=a%b);
+	}	
+}
